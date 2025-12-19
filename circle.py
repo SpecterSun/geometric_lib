8753732cc7b902b38777afaf02ef84d42e00c275
SpecterSun <jekaff07@gmail.com>
2025-12-19 12:24:26 +0600

    tests

--- circle.py
+++ circle.py
@@ -2,9 +2,10 @@ import math
 
 
 def area(r):
+    '''Принимает радиус окружности r, возращает площадь окружности pi*r*r'''
     return math.pi * r * r
 
-
 def perimeter(r):
+    '''Принимает радиус окружности r, возращает периметр окружности 2*pi*r'''
     return 2 * math.pi * r
 
